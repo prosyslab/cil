ebf8a46a18561903d81428d2ae545cc3bd3d0aa8
Kihong Heo <kheo@cis.upenn.edu>
2018-07-20 20:25:30 +0000

    [Refactoring] replace deprecated APIs

--- src/frontc/cabs2cil.ml
+++ src/frontc/cabs2cil.ml
@@ -3417,7 +3417,7 @@ and doExp (asconst: bool)   (* This expression is used as a constant *)
           let l = String.length str in
           fun s ->
             let ls = String.length s in
-            l >= ls && s = String.uppercase (String.sub str (l - ls) ls)
+            l >= ls && s = String.uppercase_ascii (String.sub str (l - ls) ls)
         in
         match ct with
           A.CONST_INT str -> begin
